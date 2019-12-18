d9e9cf0260c11a77fe44a9d3bf6dac45fb499525
Carlos Julio Pérez Quizhpe <carlosjulioperez@gmail.com>
2019-12-18 18:23:21 -0500

    Agregar csv

---
+++ etc/sql/query.sql
@@ -0,0 +1,2 @@
+--SELECT * FROM NOMINA.PROVINCIA;
+SET table nomina.provincia source "./etc/csv/provincia.csv"
--- src/ec/carper/nomina/model/Empleado.groovy
+++ src/ec/carper/nomina/model/Empleado.groovy
@@ -1,27 +1,42 @@
 package ec.carper.nomina.model
 
+import java.math.BigDecimal;
+import java.util.List;
+
 import ec.carper.nomina.model.Ciudad
 import ec.carper.nomina.model.Provincia
+import java.time.LocalDate
 import javax.persistence.*
+import javax.validation.constraints.Digits
 import org.openxava.annotations.*
+import org.openxava.calculators.*
 import org.openxava.model.*
 
 @Entity
 @Tab(properties="""cedula,apellidos,nombres,email""")
 @View(members=  """
     tabDatosPersonales{
-        cedula;
+        cedula, fechaNacimiento;
         apellidos,nombres;
         email;
         provincia,ciudad;
-        direccion,
-        telefono
+        direccion;
+        telefono;
+        estadoCivil
+    }
+    tabSueldo{
+        departamento;
+        sueldo
     }
 """)
 class Empleado extends Identifiable{
 
     @Column(length=10) @Required
     String cedula
+
+    @DefaultValueCalculator(CurrentLocalDateCalculator.class)
+    @Required
+    LocalDate fechaNacimiento
 
     @Column(length=40) @Required
     String apellidos
@@ -45,4 +60,17 @@ class Empleado extends Identifiable{
 
     @Column(length=20) @Required
     String telefono
+    
+    @ManyToOne(fetch=FetchType.LAZY)
+    @DescriptionsList
+    EstadoCivil estadoCivil
+    
+    @ManyToOne(fetch=FetchType.LAZY)
+    @DescriptionsList
+    Departamento departamento
+
+    //@Digits(integer=4,fraction=2) 
+    @Stereotype("MONEY") @Required
+    BigDecimal sueldo
+
 }
